2b01624bc9288eb09df5be2904ccf349ed2e2649
raina08 <73424213+raina08@users.noreply.github.com>
2020-12-06 13:10:10 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,18 @@
+class Box{
+    constructor(x,y,width,height){
+        var options={
+            restitution:0.3
+        }
+        this.body=Bodies.rectangle(x,y,width,height,options)
+        World.add(world,this.body) 
+     }
+     display(){
+         var pos=this.body.position
+         var angle=this.body.angle
+         push ()
+         translate(pos.x,pos.y)
+         rectMode(CENTER)
+         rect(0,0,this.width,this.height)
+         pop ()
+     }
+}
